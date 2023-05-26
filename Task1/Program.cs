4307fb98a8ba85754f00dd045aa68cd81f63c25b
Egor.S <Egorsoliakov@gmail.com>
2023-05-26 15:10:48 +0400

    Final

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M 
+//чисел. Посчитайте, сколько чисел больше 0 ввёл 
+//пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа: ");
+string[] nums_strings = Console.ReadLine().Split();
+int[] nums = new int[nums_strings.Length];
+for(int i = 0; i < nums_strings.Length; i++)
+    nums[i] = Convert.ToInt32(nums_strings[i]);
+Console.WriteLine(nums);
+Console.Write($"  ->{PositiveNumbers(nums)}");
+
+
+int PositiveNumbers(int[] arr)
+{
+    int count = 0;
+     for(int i =0; i<arr.Length; i++)
+     
+     if(arr[i]>0)
+     {  
+        count = count +1;
+     }
+     return count;
+} 
+
+// Все работает, но не понимаю как в консоле, после запуска, убрать "System.Int32[]".
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая 
+//найдёт точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются 
+// пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Дано два уравнения y = k1 * x + b1, y = k2 * x + b2 ");
+double b1 = Prompt("Введите значение b1: ");
+double k1 = Prompt("Введите значение k1: ");
+double b2 = Prompt("Введите значение b2: ");
+double k2 = Prompt("Введите значение k2: ");
+PointCoordinate(b1, k1, b2, k2);
+
+
+double Prompt(string messege)
+
+{
+    Console.Write(messege);
+    int coordinate = int.Parse(Console.ReadLine());
+    double number = Convert.ToDouble(coordinate);
+    return number;
+}
+
+void PointCoordinate(double b1, double k1, double b2, double k2)
+{
+    double coordX = (b1-b2)/(k2-k1);
+    double coordY = k1*((b1-b2)/(k2-k1)) + b1;
+    Console.WriteLine($"Точка пересечения двух прямых имеет координаты: ({coordX}; {coordY})");
+}
